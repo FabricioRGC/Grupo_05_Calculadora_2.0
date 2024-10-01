82127624a1f6a36167e8913fd9e32f4089c3e59a
yaritza <brighytp18@gmail.com>
2024-10-01 16:05:56 -0500

    holaaaa

--- Grupo_05_Calculadora/Form1.cs
+++ Grupo_05_Calculadora/Form1.cs
@@ -50,8 +50,7 @@ namespace Grupo_05_Calculadora
             }
             else if (rb_Divicion.Checked)
             {
-                MessageBox.Show("Ejecucion no validad, por falta de codigo.");
-                /*if (segundoNumero != 0)
+                if (segundoNumero != 0)
                 {
                     resultado = primerNumero / segundoNumero;
                     operacion = " / ";
